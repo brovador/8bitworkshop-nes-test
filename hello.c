ab0150007100cc1269c8c4922d2d5472cfd301ac
Jesús Bartús <jesus.bartus@gmail.com>
2019-09-08 12:36:43 +0200

    moving content to states with logic inside

--- hello.c
+++ hello.c
@@ -8,55 +8,96 @@ Finally, turn on the PPU to display video.
 
 #include "neslib.h"
 
+#define STATE_START 0
+#define STATE_GAME  1
+
 // link the pattern table into CHR ROM
 //#link "chr_generic.s"
 
 // main function, run after console reset
-static unsigned int x = 0;
 static unsigned char VRAM_BUFFER[32];
+
+static unsigned char pad;
+static unsigned char state = STATE_START;
 
 void nmi_handler()
 {
   set_vram_update(VRAM_BUFFER);
-  scroll(x, 0);
+  //scroll(x, 0);
 }
 
-void main(void) {
 
-  // set palette colors
+void state_start_loop()
+{
+  //State enter
+  VRAM_BUFFER[0] = MSB(NTADR_A(2, 2));
+  VRAM_BUFFER[1] = LSB(NTADR_A(2, 2));
+  VRAM_BUFFER[2] = 'S';
+  VRAM_BUFFER[3] = NT_UPD_EOF;
+  
+  //State update
+  while(1) {
+    
+    //Logic suff
+    if (nesclock() % 30 == 0) {
+      VRAM_BUFFER[2] = VRAM_BUFFER[2] == 'S' ? ' ' : 'S';
+    }
+    
+    //nmi sync
+    ppu_wait_nmi();
+    
+    //Read pad
+    pad = pad_trigger(0);
+    if (pad & PAD_START) {
+      state = STATE_GAME;
+      break;
+    }
+  }
+  
+  //State exit
+  //TODO...
+}
+
+
+void state_game_loop()
+{
+  //State enter
+  VRAM_BUFFER[0] = MSB(NTADR_A(2, 2));
+  VRAM_BUFFER[1] = LSB(NTADR_A(2, 2));
+  VRAM_BUFFER[2] = 'G';
+  VRAM_BUFFER[3] = NT_UPD_EOF;
+  
+  while(1) {
+    
+    //State logic
+    //TODO
+    
+    //nmi sync
+    ppu_wait_nmi();
+    
+    //Read pad
+  }
+  
+  //State exit
+  //TODO
+}
+
+
+void main(void) {
   pal_col(0,0x02);	// set screen to dark blue
   pal_col(1,0x14);	// fuchsia
   pal_col(2,0x20);	// grey
   pal_col(3,0x30);	// white
 
-  // write text to name table
-  //vram_adr(NTADR_A(2,2));		// set address
-  //vram_write("HELLO, WORLD!", 13);	// write bytes to video RAM
-  
-  VRAM_BUFFER[0] = MSB(NTADR_A(2, 2));
-  VRAM_BUFFER[1] = LSB(NTADR_A(2, 2));
-  VRAM_BUFFER[2] = '0';
-  VRAM_BUFFER[3] = NT_UPD_EOF;
-  
   nmi_set_callback(nmi_handler);
-
-  // enable PPU rendering (turn on screen)
   ppu_on_all();
-
+  
   // infinite loop
   while (1) {
-    
-    //Do logic stuff...
-    
-    //Update print area
-    x += 1;
-    if (x % 30) {
-      VRAM_BUFFER[0] = MSB(NTADR_A(0 + x / 30, 1));
-      VRAM_BUFFER[1] = LSB(NTADR_A(0 + x / 30, 1));
+    if (state == STATE_START) {
+    	state_start_loop();
+    } else if (state == STATE_GAME) {
+      	state_game_loop();
     }
-    
-    ppu_wait_nmi();
-    //Read pad...
-    
   }
 }
